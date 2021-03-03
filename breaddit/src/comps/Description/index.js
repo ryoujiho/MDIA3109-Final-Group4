5be7e9692685cb79d9e906c5a9e7f747bdc944d3
mabeliang <mabeliang@hotmail.com>
2021-03-02 17:16:38 -0800

    created comps for userpic, upload, and description

--- breaddit/src/comps/Description/index.js
+++ breaddit/src/comps/Description/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const DescriptionBox = styled.textarea`
+    width:290px;
+    height:80px;
+    background:#f8f8f8;
+    padding: 10px 10px;
+    border-radius:4px;
+    border: 1px solid white;
+    outline:none;
+    resize:none;
+`;
+
 const Description = () => {
     return <div>
-
+        <DescriptionBox></DescriptionBox>
     </div>
 }
 
--- breaddit/src/comps/Profile/Userpic/index.js
+++ breaddit/src/comps/Profile/Userpic/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const UserPicIcon = styled.div`
+    width:111px;
+    height:111px;
+    border-radius:100%;
+    background:#C4C4C4;
+`;
+
+const UserUnderline = styled.div`
+    width:98px;
+    height:0px;
+    border: 2px solid #92BFB1;
+`
+
 const Userpic = () => {
     return <div>
-
+        <UserPicIcon></UserPicIcon>
+        <UserUnderline></UserUnderline>
     </div>
 }
 
