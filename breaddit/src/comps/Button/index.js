033c3f14390b713866da1a05da910293486631c3
“rkdms9608” <rkdms9608@gmail.com>
2021-03-07 11:05:53 -0800

    btn, comment input, divider comps

--- breaddit/src/comps/Button/index.js
+++ breaddit/src/comps/Button/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = () => {
-    return <div>
+const ButtonContainer = styled.button`
+    width: ${props =>props.width ? props.width : '200px'};
+    height: ${props =>props.height ? props.height : '45px'};
+    background: ${props =>props.bgcolor ? props.bgcolor : '#F4AC45'};
+    border-radius: ${props =>props.radius ? props.radius : '5px'};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border: none;
+    cursor: pointer;
+`;
 
-    </div>
+const ButtonText = styled.p`
+    text-align: center;
+    font-size: ${props =>props.fontSize ? props.fontSize : '18px'};
+    color:${props =>props.textColor ? props.textColor : '#FFFFFF'};
+`;
+
+const Button = ({onClick, disabled, text, textColor, bgcolor, width, height, radius, fontSize}) => {
+    return <ButtonContainer onClick={onClick} disabled={disabled} bgcolor = {bgcolor} height={height} width={width} radius={radius}>
+            <ButtonText fontSize={fontSize} textColor = {textColor}>{text}</ButtonText>
+        </ButtonContainer>
 }
 
 Button.defaultProps = {
-
-
+    color: null,
+    disabled: true,
+    text: "Button"
 }
 
 export default Button;
+
---
+++ breaddit/src/comps/Input/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InputBox = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+// const InputLabel = styled.label`
+//     font-family: Roboto;
+//     font-size: 18px;
+//     color: #676767;
+// `;
+
+const InputCont = styled.input`
+    min-height: 42px;
+    min-width: 350px;
+    margin-top: 4px;
+    border-radius: 5px;
+    ::placeholder{
+        font-family: Roboto;
+        font-size: 18px;
+        color: #C4C4C4} ;
+    border:solid 1px #DDD9D9;
+`;
+
+
+const Input = ({placeholder}) => {
+    return <div>
+        <InputBox>
+            {/* <InputLabel>{text}</InputLabel> */}
+            <InputCont placeholder={placeholder}/>
+        </InputBox>
+    </div>
+}
+
+Input.defaultProps = {
+    userphoto: "public/UserIcon.png",
+    placeholder: 'Add a comment'
+}
+
+export default Input;
+
