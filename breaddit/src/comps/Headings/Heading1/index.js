39c6abd2d73d3f76616c2ae4fa360678397ffeae
叶伊婧 <yeyijing@yeyijingdeMacBook-Pro.local>
2021-03-07 10:22:30 -0800

    comps done

--- breaddit/src/comps/Headings/Heading1/index.js
+++ breaddit/src/comps/Headings/Heading1/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+const HeadingText = styled.div`
+font-family: Lato;
+font-style: normal;
+font-weight: bold;
+font-size: 36px;
+line-height: 43px;
+text-align: center;
+
+color: #0B0B09;
+
+`
+
 const Heading1 = () => {
-    return <h1>
-        Hi I'm testing
-    </h1>
+    return <div>
+    <HeadingText>Heading1</HeadingText>
+    </div>
 }
 
 Heading1.defaultProps = {
--- breaddit/src/comps/Profile/Userinfo/index.js
+++ breaddit/src/comps/Profile/Userinfo/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const UserText = styled.div`
+font-family: Lato;
+font-style: normal;
+font-weight: bold;
+font-size: 40px;
+line-height: 48px;
+/* identical to box height */
+display: flex;
+align-items: center;
+text-align: center;
+color: #F4AC45;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const UserSmall = styled.div`
+font-family: Lato;
+font-style: normal;
+font-weight: normal;
+font-size: 16px;
+line-height: 19px;
+text-align: center;
+
+color: #0B0B09;
+
+`
+
+
 const Userinfo = () => {
     return <div>
-
+        <UserText>Username</UserText>
+        <UserSmall>Since 22/12/25</UserSmall>
     </div>
 }
 
