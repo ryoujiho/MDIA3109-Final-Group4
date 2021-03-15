4165e3bbae551cfeb2084ba28cae11b0a484e083
= <=>
2021-03-15 14:06:28 -0700

    Posting / Sign up

--- breaddit/src/pages/CreatePost/index.js
+++ breaddit/src/pages/CreatePost/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Topbar from 'comps/Topbar';
 import Description from 'comps/Description';
 import Upload from 'comps/Upload';
@@ -11,19 +11,62 @@ import {
     Route,
     Link
   } from "react-router-dom";
+  import axios from 'axios';
+  import {useHistory} from 'react-router-dom';
   
 
   const CreatePost = () => {
+    const [user, setUser] = useState("");
+    const [file, setFile] = useState();
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState()
+
+    const submit = async event => {
+      event.preventDefault()
+
+      const formData = new FormData()
+      formData.append("image", file)
+      formData.append("description", description)
+
+      const result = await axios.post('/api/posts', formData, {headers: {
+        'Content-Type': 'multipart/form-data'
+      }})
+      console.log(result.data)
+      setImage(result.data.imagePath)
+      
+    }
+
+    const CheckToken = async () => {
+        const resp = await axios.post("/api/verify");
+        console.log(resp.data);
+        if(resp.data !== "no token sent to server" || "Invalid Token") {
+            setUser(resp.data)
+        }
+    }
+
+    useEffect(()=> {
+        CheckToken();
+    }, [])
+
     return <div className="pagebody">
         <Topbar pageName="Create Post"/>
-        <Userpic></Userpic>
-        <div className="postform">
+        <Userpic username={user.profile_photo}/>
+        
+        <form className="postform" onSubmit={submit}>
           <Heading2 text="Pick a Photo"/>
-          <Upload></Upload>
+          
+          <Upload filename={file} onChange={(e) =>
+            setFile(e.target.files[0])
+          }/>
+
           <Heading2 text="Add a Description"/>
-          <Description></Description>
-        </div>
-        <Button text="Post It!" width="82%"/>
+          
+          <Description onChange={(e) => 
+            setDescription(e.target.value)}/>
+        
+        <Button text="Post It!" width="100%" type="submit" onClick={submit}/>
+        </form>
+          {/* {imagePath && <img src={imagePath} />} */}
     </div>
   }
 
